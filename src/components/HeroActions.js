b3ba8852fceaf879397dc336db82d38c6ae7286d
brandotheturner <brandontheturner@gmail.com>
2019-04-23 18:18:38 -0500

    setting up actions

--- src/components/HeroActions.js
+++ src/components/HeroActions.js
@@ -13,4 +13,66 @@ class HeroActions extends Component {
     };
 };
 
-handleText
+handleText = e => {
+    const newState = { ...this.state }
+    newState[e.target.id] = e.target.value
+    this.setState(newState)
+}
+
+createHero = (e) => {
+    e.preventDefault()
+    fetch('/api/heros', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            heroName: this.state.createHeroName,
+            heroDescription: this.state.createHeroDescription,
+            heroPowers: this.state.createHeroPowers,
+            deleteHero: this.state.deleteHeroName
+        })
+    })
+    .then(res => res.json())
+    .then(result => this.PaymentResponse.createHero(result))
+    .then(() => {
+        this.setState({
+            createHeroName: '',
+            createHeroDescription: '',
+            createHeroPowers: '',
+            deleteHeroName: ''
+        })
+    })
+}
+
+deleteHero = e => {
+    e.preventDefault()
+    fetch(`/api/heros/${this.state.deleteHeroName}`, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(result => this.props.deleteHeroName(this.state.deleteHeroName))
+    .then(() => {
+        this.setState({
+            deleteHeroName: ''
+        })
+    })
+}
+
+render() {
+    return (
+        <div>
+            <FlexForm onSubmit={this.createHero}>
+            <TextField onChange={this.handleTextChange} id="createHeroName"
+            label="Hero Name"
+            value={this.state.createHeroName}
+            variant="outlined" />
+            
+            </FlexForm>
+        </div>
+    )
+}
+
