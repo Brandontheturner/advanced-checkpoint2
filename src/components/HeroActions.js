6639d80571130e10629463d4d777b9cdb80416dc
brandotheturner <brandontheturner@gmail.com>
2019-04-22 21:37:44 -0500

    adding components

---
+++ src/components/HeroActions.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import styling from symantic or materials UI
+// additional styling options
+
+// styled component, if wanted
+
+class HeroActions extends Component {
+    state = {
+        createHeroName: '',
+        createHeroDescription: '',
+        createHeroPowers: '',
+        deleteHeroName: '' 
+    };
+};
+
+handleText
